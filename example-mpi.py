a0e592b3d3dabb6f4b05de89f8b0dcfc0ad82933
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2017-01-19 16:23:13 -0800

    updated example-mpi.py for python 3

--- example-mpi.py
+++ example-mpi.py
@@ -7,11 +7,11 @@ comm = MPI.COMM_WORLD
 
 # simple print out Rank & Size
 id = comm.Get_rank()
-print "Of ", comm.Get_size() , " workers, I am number " , id, "."
+print("Of ", comm.Get_size() , " workers, I am number " , id, ".")
 
 def f(id, n):
     np.random.seed(id)
-    return np.mean(np.random.normal(0, 1, n))
+    return(np.mean(np.random.normal(0, 1, n)))
 
 n = 1000000
 result = f(id, n)
@@ -20,4 +20,4 @@ result = f(id, n)
 output = comm.gather(result, root = 0)
 
 if id == 0:
-    print output
+    print(output)
